707a936494d358b1525ce8dc519ffc360e754ddc
Scarlet <nn814921@gmail.com>
2025-04-20 22:37:02 +0300

    item that slows time

--- Assets/Resources/Scripts/Items/TimeScale_Item.cs
+++ Assets/Resources/Scripts/Items/TimeScale_Item.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class TimeScale_Item : Item
 {
-    [SerializeField]
-    private float timeDelta;
+    [Header("Settings")]
+    [SerializeField, Range(0.1f, 1f)]
+    private float minTimeScale = 0.5f;
+
+    [SerializeField, Range(1.1f, 3f)]
+    private float slowdownFactor = 3f;
+
     protected override void UseItem()
     {
-        TimeManipulator.instance.currentTimeScale -= timeDelta;
-        TimeManipulator.instance.AccelerateTime();
+        float newTimeScale = TimeManipulator.instance.currentTimeScale / slowdownFactor;
+
+
+        newTimeScale = Mathf.Max(minTimeScale, newTimeScale);
+
+
+        TimeManipulator.instance.currentTimeScale = newTimeScale;
+        Time.timeScale = newTimeScale;
+
+
+        TimeManipulator.instance.UpdateUI();
+
+        TimeManipulator.instance.lastAccelerationTime = Time.unscaledTime;
+
     }
 }

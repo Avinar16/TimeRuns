0bce9a50a81fa203f2bab83e19687dd93d18edb9
Avinar16 <74549348+Avinar16@users.noreply.github.com>
2025-04-20 03:36:50 +0300

    Added shield

--- Assets/Resources/Scripts/Entities/Entity.cs
+++ Assets/Resources/Scripts/Entities/Entity.cs
@@ -1,6 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections;
 
 public abstract class Entity : MonoBehaviour
 {
@@ -39,8 +40,25 @@ public abstract class Entity : MonoBehaviour
     }
     public virtual void TakeKnockBack(Vector2 direction, float distance)
     {
-        Vector2 targetPos = new Vector2(transform.position.x, transform.position.y) + direction * distance;
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, distance);
+        //Vector2 targetPos = new Vector2(transform.position.x, transform.position.y) + direction * distance;
+        //transform.position = Vector2.MoveTowards(transform.position, targetPos, distance);
+        StartCoroutine(KnockBackRoutine(direction, distance));
+    }
+    private IEnumerator KnockBackRoutine(Vector2 direction, float distance, float duration = 0.25f)
+    {
+        Vector2 startPos = transform.position;
+        Vector2 targetPos = startPos + direction * distance;
+
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector2.Lerp(startPos, targetPos, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos; // Финализируем позицию
     }
     abstract protected void Move();
 }
